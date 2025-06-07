5986d06b096fb12e4c7be4f1328843f353617e6e
kanori1022 <i.like_96neko54@ezweb.ne.jp>
2025-06-07 19:37:52 +0900

    DB情報登録処理の追加

--- app/controllers/combination_controller.rb
+++ app/controllers/combination_controller.rb
@@ -1,7 +1,31 @@
 class CombinationController < ApplicationController
-  # 一覧
+  # GET /combinations
   def index
-        conbination = Combination.find(1)
-    render json: conbination
+    combination = Combination.find(1)
+    render json: combination
+  end
+
+  # POST /combinations
+  def create
+    combination = Combination.new(combination_params)
+    # user_idを "1" に仮置きしている
+    combination.user_id = 1
+    puts "----------------------------------------------"
+    puts "combination.title: #{combination.title}"
+    puts "combination.user_id: #{combination.user_id}"
+    puts "----------------------------------------------"
+    # combination.save
+    if combination.save
+      render json: combination, status: :created
+    else
+      render json: combination.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # 受け付けるパラメータを指定
+  def combination_params
+    params.require(:combination).permit(:title, :flight, :shaft, :barrel, :tip, :description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/combinations", to: "combination#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  post "/combinations", to: "combination#create"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -12,3 +13,4 @@ Rails.application.routes.draw do
 end
 
 # rails g model User firebase_uid:string name:string
+
