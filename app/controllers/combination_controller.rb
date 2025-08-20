d290c8fc50dbf344c5447b038c58ebdaccd6d58f
kanori1022 <>
2025-08-20 15:40:29 +0900

    検索方法をAPIからデータ取得に変更

--- app/controllers/combination_controller.rb
+++ app/controllers/combination_controller.rb
@@ -1,5 +1,5 @@
 class CombinationController < ApplicationController
-skip_before_action :authenticate_user, only: [:index, :show, :newest]
+skip_before_action :authenticate_user, only: [:index, :show, :newest, :search]
 
 # GET /combinations (人気順)
 def index
@@ -18,6 +18,36 @@ end
 # GET /combinations/newest
 def newest
   combinations = Combination.order(created_at: :desc).with_attached_image
+
+  render json: combinations.map { |c|
+    c.as_json.merge(
+      image: c.image.attached? ? url_for(c.image) : nil
+    )
+  }
+end
+
+# GET /combinations/search - 検索機能
+def search
+  search_word = params[:searchWord] || ""
+  tags = params[:tags] || ""
+
+  combinations = Combination.all.with_attached_image
+
+  if search_word.present?
+    combinations = combinations.where(
+      "title LIKE ? OR description LIKE ? OR flight LIKE ? OR shaft LIKE ? OR barrel LIKE ? OR tip LIKE ?",
+      "%#{search_word}%", "%#{search_word}%", "%#{search_word}%", "%#{search_word}%", "%#{search_word}%", "%#{search_word}%"
+    )
+  end
+
+  if tags.present?
+    combinations = combinations.where("description LIKE ?", "%#{tags}%")
+  end
+
+  # 人気順でソート
+  combinations = combinations.joins("LEFT JOIN favorites ON favorites.combination_id = combinations.id")
+                            .group("combinations.id")
+                            .order("COUNT(favorites.id) DESC")
 
   render json: combinations.map { |c|
     c.as_json.merge(
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # get "combination/index"
   get "/combinations", to: "combination#index"
   get "/combinations/newest", to: "combination#newest"
+  get "/combinations/search", to: "combination#search"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get "/combinations/:id", to: "combination#show"
